85f4f7b8dc632331452531d3675e3ff100e391cb
Noraize <67844202+Noraize@users.noreply.github.com>
2023-07-11 00:19:53 +0500

    Ad Mob Integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:scanner_app/login.dart';
 import 'firebase_options.dart';
 
-void main() async {
+void main() {
+  var devices = ["A4D6C48997DFDCE4691FD0C020418DA7"];
   WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
+  RequestConfiguration requestConfiguration =
+      RequestConfiguration(testDeviceIds: devices);
+  MobileAds.instance.updateRequestConfiguration(requestConfiguration);
   Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
