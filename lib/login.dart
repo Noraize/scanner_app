871fb0c1795dc721f3a9ab148df768108bed2a8e
Noraize <67844202+Noraize@users.noreply.github.com>
2023-07-11 10:42:57 +0500

    Revert "Merge branch 'loginUI' of https://github.com/Noraize/scanner_app into loginUI"
    This reverts commit f67372ed9e297b08c39aaf4329c8d99627d02b66, reversing
    changes made to 16f73994d5b0d4d86d9588a1488e7b95ce6ea49e.

--- lib/login.dart
+++ lib/login.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:scanner_app/appbar.dart';
 import 'package:scanner_app/firebaselogin.dart';
+import 'package:scanner_app/register.dart';
 
 final emailController = TextEditingController();
 final passwordController = TextEditingController();
@@ -12,7 +13,6 @@ class loginscreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        //resizeToAvoidBottomInset: false,
         backgroundColor: Colors.white,
         appBar: NewAppBar(),
         body: Center(
@@ -23,13 +23,13 @@ class loginscreen extends StatelessWidget {
                 child: Stack(children: [
                   Container(
                     color: const Color(0xfffd7e14),
-                    height: MediaQuery.of(context).size.width / 1.5,
+                    height: MediaQuery.of(context).size.width / 2,
                   ),
-                  Image(
-                    width: 500,
-                    height: 300,
-                    image: AssetImage('assets/img/logo.png'),
-                  ),
+                  // const Image(
+                  //   width: 500,
+                  //   height: 300,
+                  //   image: AssetImage('assets/img/logo.png'),
+                  // ),
                 ]),
               ),
               const SizedBox(
@@ -38,17 +38,15 @@ class loginscreen extends StatelessWidget {
               const Text(
                 "LOGIN",
                 style: TextStyle(
-                  color: Color(0xfffd7e14),
-                  fontFamily: 'LEMONMILK-Regular',
-                  fontSize: 40,
-                  shadows: <Shadow>[
-                    Shadow(
-                      color: Color.fromARGB(255, 186, 185, 184),
-                      blurRadius: 0.5,
-                      offset: Offset(4, 4),
-                    ),
-                  ],
-                ),
+                    color: Color(0xfffd7e14),
+                    fontFamily: 'LEMONMILK-Regular',
+                    fontSize: 40,
+                    shadows: <Shadow>[
+                      Shadow(
+                          color: Color.fromARGB(255, 186, 185, 184),
+                          blurRadius: 0.5,
+                          offset: Offset(4, 4)),
+                    ]),
               ),
               const SizedBox(
                 height: 50,
@@ -60,21 +58,18 @@ class loginscreen extends StatelessWidget {
                   decoration: const InputDecoration(
                     hintText: "Email Address",
                     focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color(0xfffd7e14),
-                        width: 0.5,
-                      ),
-                    ),
+                        borderSide: BorderSide(
+                      color: Color(0xfffd7e14),
+                      width: 0.5,
+                    )),
                     border: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color(0xfffd7e14),
-                        width: 0.5,
-                      ),
-                      borderRadius: BorderRadius.horizontal(
-                        left: Radius.circular(20),
-                        right: Radius.circular(20),
-                      ),
-                    ),
+                        borderSide: BorderSide(
+                          color: Color(0xfffd7e14),
+                          width: 0.5,
+                        ),
+                        borderRadius: BorderRadius.horizontal(
+                            left: Radius.circular(20),
+                            right: Radius.circular(20))),
                   ),
                   controller: emailController,
                 ),
@@ -89,11 +84,10 @@ class loginscreen extends StatelessWidget {
                   decoration: const InputDecoration(
                     hintText: "Password",
                     focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color(0xfffd7e14),
-                        width: 0.5,
-                      ),
-                    ),
+                        borderSide: BorderSide(
+                      color: Color(0xfffd7e14),
+                      width: 0.5,
+                    )),
                     border: OutlineInputBorder(
                         borderSide: BorderSide(
                           color: Color(0xfffd7e14),
@@ -112,14 +106,14 @@ class loginscreen extends StatelessWidget {
               ),
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
-                  shadowColor: Color(0xfffd7e14),
-                  backgroundColor: Color(0xfffd7e14),
+                  shadowColor: const Color(0xfffd7e14),
+                  backgroundColor: const Color(0xfffd7e14),
                   foregroundColor: Colors.white,
-                  textStyle: TextStyle(
+                  textStyle: const TextStyle(
                       color: Colors.white,
                       fontFamily: 'LEMONMILK-Regular',
                       fontSize: 20),
-                  fixedSize: Size(100, 50),
+                  fixedSize: const Size(100, 50),
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(20)),
                 ),
@@ -130,7 +124,23 @@ class loginscreen extends StatelessWidget {
                     context,
                   );
                 },
-                child: Text("Login"),
+                child: const Text("Login"),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              InkWell(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Regisration()));
+                },
+                child: const Text(
+                  "New here? Sign up",
+                  style: TextStyle(
+                    fontFamily: 'LEMONMILK-Regular',
+                    color: Color(0xfffd7e14),
+                  ),
+                ),
               )
             ],
           ),
