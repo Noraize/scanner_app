4f8e0e3c370f74403423c5e51b122f781030dea2
Noraize <67844202+Noraize@users.noreply.github.com>
2023-07-11 10:58:30 +0500

    Revert "Revert "Ad Mob Integration""
    This reverts commit 2cd56e538c82bbaba7cc750f42fb3f9cfb92f356.

---
+++ lib/google_adsense.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+late BannerAd bannerAd;
+bool isAdLoaded = false;
+
+initBannarAd() {
+  bannerAd = BannerAd(
+    size: AdSize.banner,
+    adUnitId: "ca-app-pub-3940256099942544/6300978111",
+    listener: BannerAdListener(
+      onAdLoaded: (ad) {
+        isAdLoaded = true;
+        debugPrint("No Error" + isAdLoaded.toString());
+      },
+      onAdFailedToLoad: (ad, error) {
+        ad.dispose();
+        debugPrint("The Error is $error");
+      },
+    ),
+    request: const AdRequest(),
+  );
+  bannerAd.load();
+}
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:scanner_app/appbar.dart';
+import 'package:scanner_app/google_adsense.dart';
 import 'package:scanner_app/scanner.dart';
 
-class Hello extends StatelessWidget {
-  const Hello({super.key});
+class homepage extends StatefulWidget {
+  const homepage({super.key});
+
+  @override
+  State<homepage> createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
+  void initState() {
+    super.initState();
+    initBannarAd();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,6 +40,13 @@ class Hello extends StatelessWidget {
             ],
           ),
         ),
+        bottomNavigationBar: isAdLoaded
+            ? SizedBox(
+                height: bannerAd.size.height.toDouble(),
+                width: bannerAd.size.width.toDouble(),
+                child: AdWidget(ad: bannerAd),
+              )
+            : const SizedBox(),
       ),
     );
   }
