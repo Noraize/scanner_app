bf7e46f6ba71d0232956cbd6bac20a5b5d369d74
Noraize <67844202+Noraize@users.noreply.github.com>
2023-08-18 17:24:07 +0500

    Animations & Sounds
    Tick Animation & Sound

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:scanner_app/appbar.dart';
 import 'package:scanner_app/google_adsense.dart';
-import 'package:scanner_app/login.dart';
 import 'package:scanner_app/scanner.dart';
 
 bool isAdLoaded = false;
@@ -59,29 +58,6 @@ class _homepageState extends State<homepage> {
                   );
                 },
                 child: const Text("QR Code Scanner"),
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  shadowColor: const Color(0xfffd7e14),
-                  backgroundColor: const Color(0xfffd7e14),
-                  foregroundColor: Colors.white,
-                  textStyle: const TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'LEMONMILK-Regular',
-                      fontSize: 20),
-                  fixedSize: const Size(200, 40),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                ),
-                onPressed: () async {
-                  //Code To Sign Out From SQL
-                  Navigator.pop(
-                    context,
-                    const loginscreen(),
-                  );
-                },
-                child: const Text("Logout"),
               ),
             ],
           ),
--- lib/qrresult.dart
+++ lib/qrresult.dart
@@ -1,26 +1,61 @@
-import 'dart:html';
 import 'package:flutter/material.dart';
 import 'package:scanner_app/appbar.dart';
 import 'package:scanner_app/checkin.dart';
+import 'package:animated_check/animated_check.dart';
+import 'package:audioplayers/audioplayers.dart';
 
-class qrResult extends StatelessWidget {
+class qrResult extends StatefulWidget {
   final String? qrData;
   const qrResult({super.key, required this.qrData});
 
   @override
-  Widget build(BuildContext context) {
-    var auth = checkin(uuidextract());
+  State<qrResult> createState() => _qrResultState();
+}
 
+class _qrResultState extends State<qrResult>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+  final player = AudioPlayer();
+
+  @override
+  void initState() {
+    super.initState();
+    player.play(AssetSource("sounds/tick.mp3"));
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    _animation = Tween<double>(begin: 0, end: 1).animate(CurvedAnimation(
+        parent: _animationController, curve: Curves.easeInOutCirc));
+    _animationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    checkin(uuidextract());
     return MaterialApp(
       home: Scaffold(
         appBar: NewAppBar(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              AnimatedCheck(
+                progress: _animation,
+                size: 200,
+                color: const Color(0xfffd7e14),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
 
   String? uuidextract() {
     const substringToRemove = 'https://festifyer.com/checkin/';
-    final resultString = qrData?.replaceAll(substringToRemove, '');
+    final resultString = widget.qrData?.replaceAll(substringToRemove, '');
     return resultString;
   }
 }
